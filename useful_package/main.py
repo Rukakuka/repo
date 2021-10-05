7bd0307c9d9117d2fbc13ee4926c3437d90aff43
Maksim <katerishich.miu@gmail.com>
2021-10-05 18:42:02 +0300

    add regressor

---
+++ useful_package/main.py
@@ -0,0 +1,9 @@
+import hyperbola from module_b_dev
+import polynom3 from module_a_dev
+
+def sklearn_RandomForestRegressor(x)
+    if (x>0)
+        return hyperbola(x)
+    else
+        return polynom3(x)
+
